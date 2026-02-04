bdd5b14cb4b6f20fdff3bb7da2e15baf360d628b
Iurie Praporscic <iuri.praporscic@amdaris.com>
2026-02-04 15:47:54 +0200

    add serilog

--- Clean.Architecture/Clean.Architecture.Presentation/Program.cs
+++ Clean.Architecture/Clean.Architecture.Presentation/Program.cs
@@ -1,3 +1,5 @@
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +7,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Host.UseSerilog((ctx, cfg) => cfg.ReadFrom.Configuration(ctx.Configuration));
 
 var app = builder.Build();
 

e7ad077ac3ba35c403549104af752edd8e4db9fa
Iurie Praporscic <iuri.praporscic@amdaris.com>
2026-02-05 11:09:06 +0200

    register mediatr

---
+++ Clean.Architecture/Clean.Architecture.Application/Extensions/ServiceRegistrationExtension.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+using System.Data;
+using System.Reflection;
+
+namespace Clean.Architecture.Application.Extensions
+{
+    public static class ServiceRegistrationExtension
+    {
+        public static IServiceCollection ApplicationLayerRegistration(this IServiceCollection services)
+        {
+            services.AddMediatR(_ => _.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+            return services;
+        }
+    }
+}
--- Clean.Architecture/Clean.Architecture.Presentation/Program.cs
+++ Clean.Architecture/Clean.Architecture.Presentation/Program.cs
@@ -1,10 +1,12 @@
+using Clean.Architecture.Application.Extensions;
 using Clean.Architecture.Domain.Abstractions;
 using Clean.Architecture.Infrastructure;
 using Clean.Architecture.Infrastructure.Repositories;
 using Clean.Architecture.Presentation.Middleware;
+using Microsoft.EntityFrameworkCore;
 using Serilog;
 using System.Data;
-using Microsoft.EntityFrameworkCore;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +15,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+//swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 builder.Host.UseSerilog((ctx, cfg) => cfg.ReadFrom.Configuration(ctx.Configuration));
 
@@ -26,12 +32,21 @@ builder.Services.AddScoped<IUnitOfWork>(
 
 builder.Services.AddTransient<ExceptionHandlingMiddleware>();
 
+//builder.Services.AddMediatR(_ => _.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+builder.Services.AddScoped<IDbConnection>(
+factory => factory.GetRequiredService<ApplicationDbContext>().Database.GetDbConnection());
+
+builder.Services.ApplicationLayerRegistration();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+
+    app.UseSwagger();               // Generate Swagger JSON
+    app.UseSwaggerUI();             // Enable Swagger UI
 }
 
 app.UseHttpsRedirection();
